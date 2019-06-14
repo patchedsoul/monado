217ced762eddd133f3f76d84348dea40acc4dad0
Ryan Pavlik <ryan.pavlik@collabora.com>
2019-06-14 15:47:41 -0500

    d/mt: Re-enable UVBI builds, against new-layout UVBI.

--- src/xrt/drivers/montrack/optical_tracking/tracker3D_uvbi.cpp
+++ src/xrt/drivers/montrack/optical_tracking/tracker3D_uvbi.cpp
@@ -1,9 +1,9 @@
-#include "tracker3D_osvr_uvbi.h"
-#include "TrackingSystem.h"
-#include "TrackingDebugDisplay.h"
-#include "opencv4/opencv2/opencv.hpp"
+#include "tracker3D_uvbi.h"
+#include "unifiedvideoinertial/TrackingSystem.h"
+#include "unifiedvideoinertial/TrackingDebugDisplay.h"
+#include "opencv2/opencv.hpp"
 
-typedef struct tracker3D_osvr_uvbi_instance
+typedef struct tracker3D_uvbi_instance
 {
 	bool configured;
 	measurement_consumer_callback_func measurement_target_callback;
@@ -11,20 +11,20 @@ typedef struct tracker3D_osvr_uvbi_instance
 	event_consumer_callback_func event_target_callback;
 	void* event_target_instance; // where we send our measurements
 	tracker_mono_configuration_t configuration;
-	osvr::vbtracker::TrackingSystem* system;
-	osvr::vbtracker::TrackedBody* hmd;
-	osvr::vbtracker::TrackingDebugDisplay* debug;
+	videotracker::uvbi::TrackingSystem* system;
+	videotracker::uvbi::TrackedBody* hmd;
+	videotracker::uvbi::TrackingDebugDisplay* debug;
 	cv::Mat frame_gray;
 	cv::Mat debug_rgb;
 	bool alloced_frames;
-	osvr::vbtracker::CameraParameters camera_params;
-	osvr::util::time::TimeValue current_time;
-} tracker3D_osvr_uvbi_instance_t;
+	videotracker::CameraParameters camera_params;
+	videotracker::util::TimeValue current_time;
+} tracker3D_uvbi_instance_t;
 
 
 
 capture_parameters_t
-tracker3D_osvr_uvbi_get_capture_params(tracker_instance_t* inst)
+tracker3D_uvbi_get_capture_params(tracker_instance_t* inst)
 {
 	capture_parameters_t cp = {};
 	cp.exposure = 0.5f;
@@ -32,20 +32,19 @@ tracker3D_osvr_uvbi_get_capture_params(tracker_instance_t* inst)
 	return cp;
 }
 
-tracker3D_osvr_uvbi_instance_t*
-tracker3D_osvr_uvbi_create(tracker_instance_t* inst)
+tracker3D_uvbi_instance_t*
+tracker3D_uvbi_create(tracker_instance_t* inst)
 {
 
-	tracker3D_osvr_uvbi_instance_t* i =
-	    (tracker3D_osvr_uvbi_instance_t*)calloc(
-	        1, sizeof(tracker3D_osvr_uvbi_instance_t));
+	tracker3D_uvbi_instance_t* i = (tracker3D_uvbi_instance_t*)calloc(
+	    1, sizeof(tracker3D_uvbi_instance_t));
 	if (i) {
-		osvr::vbtracker::ConfigParams cp;
-		i->system = new osvr::vbtracker::TrackingSystem(cp);
-		i->debug = new osvr::vbtracker::TrackingDebugDisplay(cp);
+		videotracker::uvbi::ConfigParams cp;
+		i->system = new videotracker::uvbi::TrackingSystem(cp);
+		i->debug = new videotracker::uvbi::TrackingDebugDisplay(cp);
 		i->debug_rgb = cv::Mat(480, 1280, CV_8UC3, cv::Scalar(0, 0, 0));
 		i->camera_params =
-		    osvr::vbtracker::getSimulatedHDKCameraParameters();
+		    videotracker::getSimulatedHDKCameraParameters();
 		i->hmd = i->system->createTrackedBody();
 		i->alloced_frames = false;
 		return i;
@@ -55,16 +54,16 @@ tracker3D_osvr_uvbi_create(tracker_instance_t* inst)
 
 
 bool
-tracker3D_osvr_uvbi_get_debug_frame(tracker_instance_t* inst, frame_t* frame)
+tracker3D_uvbi_get_debug_frame(tracker_instance_t* inst, frame_t* frame)
 {
-	tracker3D_osvr_uvbi_instance_t* internal =
-	    (tracker3D_osvr_uvbi_instance_t*)inst->internal_instance;
+	tracker3D_uvbi_instance_t* internal =
+	    (tracker3D_uvbi_instance_t*)inst->internal_instance;
 	cv::Mat rgbFrame;
 	cv::cvtColor(internal->frame_gray, rgbFrame, CV_GRAY2BGR);
-	cv::Mat osvr_debug = internal->debug->createStatusImage(
+	cv::Mat uvbi_debug = internal->debug->createStatusImage(
 	    *(internal->system), internal->camera_params, rgbFrame);
 
-	osvr_debug.copyTo(internal->debug_rgb);
+	uvbi_debug.copyTo(internal->debug_rgb);
 
 
 	frame->format = FORMAT_RGB_UINT8;
@@ -78,10 +77,10 @@ tracker3D_osvr_uvbi_get_debug_frame(tracker_instance_t* inst, frame_t* frame)
 	return true;
 }
 bool
-tracker3D_osvr_uvbi_queue(tracker_instance_t* inst, frame_t* frame)
+tracker3D_uvbi_queue(tracker_instance_t* inst, frame_t* frame)
 {
-	tracker3D_osvr_uvbi_instance_t* internal =
-	    (tracker3D_osvr_uvbi_instance_t*)inst->internal_instance;
+	tracker3D_uvbi_instance_t* internal =
+	    (tracker3D_uvbi_instance_t*)inst->internal_instance;
 	printf("received frame, tracking!\n");
 	if (!internal->alloced_frames) {
 		internal->frame_gray = cv::Mat(frame->height, frame->stride,
@@ -101,23 +100,23 @@ tracker3D_osvr_uvbi_queue(tracker_instance_t* inst, frame_t* frame)
 	return true;
 }
 bool
-tracker3D_osvr_uvbi_get_poses(tracker_instance_t* inst,
-                              tracked_object_t* objects,
-                              uint32_t* count)
+tracker3D_uvbi_get_poses(tracker_instance_t* inst,
+                         tracked_object_t* objects,
+                         uint32_t* count)
 {
 	return false;
 }
 bool
-tracker3D_osvr_uvbi_new_poses(tracker_instance_t* inst)
+tracker3D_uvbi_new_poses(tracker_instance_t* inst)
 {
 	return false;
 }
 bool
-tracker3D_osvr_uvbi_configure(tracker_instance_t* inst,
-                              tracker_mono_configuration_t* config)
+tracker3D_uvbi_configure(tracker_instance_t* inst,
+                         tracker_mono_configuration_t* config)
 {
-	tracker3D_osvr_uvbi_instance_t* internal =
-	    (tracker3D_osvr_uvbi_instance_t*)inst->internal_instance;
+	tracker3D_uvbi_instance_t* internal =
+	    (tracker3D_uvbi_instance_t*)inst->internal_instance;
 	// return false if we cannot handle this config
 
 	if (config->format != FORMAT_Y_UINT8) {
@@ -129,14 +128,13 @@ tracker3D_osvr_uvbi_configure(tracker_instance_t* inst,
 	return true;
 }
 void
-tracker3D_osvr_uvbi_register_measurement_callback(
+tracker3D_uvbi_register_measurement_callback(
     tracker_instance_t* inst,
     void* target_instance,
     measurement_consumer_callback_func target_func)
 {}
 void
-tracker3D_osvr_uvbi_register_event_callback(
-    tracker_instance_t* inst,
-    void* target_instance,
-    event_consumer_callback_func target_func)
+tracker3D_uvbi_register_event_callback(tracker_instance_t* inst,
+                                       void* target_instance,
+                                       event_consumer_callback_func target_func)
 {}
