a9b45b470b995c21766ccfd5a16abaa239f1e636
Ryan Pavlik <ryan.pavlik@collabora.com>
2019-09-17 19:28:18 -0500

    aux/tracking: Use kalman filter in IMU fusion.

--- src/xrt/auxiliary/tracking/t_imu.cpp
+++ src/xrt/auxiliary/tracking/t_imu.cpp
@@ -9,18 +9,96 @@
  */
 
 #include "t_imu.h"
-#include "t_imu_fusion.h"
+#include "t_fusion.h"
 #include "math/m_eigen_interop.h"
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+#include "../../external/flexkalman/OrientationConstantVelocity.h"
+#include "../../external/flexkalman/AngularVelocityMeasurement.h"
+#include "../../external/flexkalman/FlexibleUnscentedCorrect.h"
+#include "../../external/flexkalman/FlexibleKalmanFilter.h"
+#include "../../external/flexkalman/PureVectorState.h"
+#include "../../external/flexkalman/ConstantProcess.h"
+#include "../../external/flexkalman/AugmentedState.h"
+#include "../../external/flexkalman/AugmentedProcessModel.h"
 
 #include <memory>
 
+using IMUProcessModel = flexkalman::OrientationConstantVelocityProcessModel;
+using IMUState = IMUProcessModel::State;
+
+using BiasState = flexkalman::PureVectorState<3>;
+using flexkalman::types::Vector;
+
+
 struct imu_fusion
 {
+	IMUState state{};
+	IMUProcessModel processModel{};
 	uint64_t time_ns{};
-
-	xrt_fusion::SimpleIMUFusion simple_fusion;
+	struct
+	{
+		BiasState state{Vector<3>::Zero(),
+		                Vector<3>::Constant(10).asDiagonal()};
+		flexkalman::ConstantProcess<BiasState> processModel{};
+	} gyro_bias;
 };
 
+/*
+ * internal functions
+ */
+
+//! Predict the filter forward, then correct using the supplied measurement as
+//! an "unscented" Kalman filter.
+template <typename MeasurementType>
+static int
+unscentedPredictCorrect(imu_fusion &fusion,
+                        float dt,
+                        MeasurementType &&meas,
+                        const char *meas_desc)
+{
+	if (dt != 0) {
+		flexkalman::predict(fusion.state, fusion.processModel, dt);
+	}
+	auto correctionInProgress =
+	    flexkalman::beginUnscentedCorrection(fusion.state, meas);
+	if (!correctionInProgress.stateCorrectionFinite) {
+		fprintf(stderr,
+		        "Non-finite state correction in applying "
+		        "%s\n",
+		        meas_desc);
+		return -2;
+	}
+
+	if (!correctionInProgress.finishCorrection(true)) {
+		fprintf(stderr, "Non-finite covariance after applying %s\n",
+		        meas_desc);
+		return -3;
+	}
+
+	return 0;
+}
+
+
+static int
+imu_fusion_output_prediction(IMUState const &state,
+                             struct xrt_quat &out_quat,
+                             struct xrt_vec3 &out_ang_vel)
+{
+	map_quat(out_quat) = state.getQuaternion().cast<float>();
+	map_vec3(out_ang_vel) = state.angularVelocity().cast<float>();
+	return 0;
+}
+
+static int
+imu_fusion_output_rot_vec(IMUState const &state, struct xrt_vec3 &out_rot_vec)
+{
+	map_vec3(out_rot_vec) =
+	    flexkalman::util::quat_ln(state.getQuaternion().cast<float>()) * 2;
+	return 0;
+}
 
 /*
  * API functions
@@ -30,6 +108,12 @@ imu_fusion_create()
 {
 	try {
 		auto fusion = std::make_unique<imu_fusion>();
+		// Arbitrarily high since we have no idea where we are.
+		fusion->state.setErrorCovariance(
+		    Vector<6>::Constant(10).asDiagonal());
+
+		fusion->processModel.setNoiseAutocorrelation(
+		    Vector<3>::Constant(1.3e-1));
 		return fusion.release();
 	} catch (...) {
 		return NULL;
@@ -57,8 +141,40 @@ imu_fusion_incorporate_gyros(struct imu_fusion *fusion,
 		assert(ang_vel);
 		assert(variance);
 
-		fusion->simple_fusion.handleGyro(
-		    map_vec3(*ang_vel).cast<double>(), dt);
+		if (dt != 0) {
+			flexkalman::predict(fusion->state, fusion->processModel,
+			                    dt);
+			// Advance the gyro bias state as well: just updates the
+			// variance
+			flexkalman::predict(fusion->gyro_bias.state,
+			                    fusion->gyro_bias.processModel, dt);
+		}
+#if 0
+	// the filter runs on doubles, not floats
+	auto meas = flexkalman::AngularVelocityMeasurement(
+	    map_vec3(*ang_vel).cast<double>(),
+	    map_vec3(*variance).cast<double>());
+	if (!flexkalman::correctUnscented(fusion->state, meas)) {
+		fprintf(stderr,
+		        "Non-finite results in correcting fusion using %s - "
+		        "update canceled.\n",
+		        meas_desc);
+		return -2;
+	}
+#else
+		auto meas = xrt_fusion::BiasedGyroMeasurement{
+		    map_vec3(*ang_vel).cast<double>(),
+		    map_vec3(*variance).cast<double>()};
+		auto augmented_state = flexkalman::makeAugmentedState(
+		    fusion->state, fusion->gyro_bias.state);
+		if (!flexkalman::correctUnscented(augmented_state, meas)) {
+			fprintf(stderr,
+			        "Non-finite results in correcting fusion using "
+			        "gyros - "
+			        "update canceled.\n");
+			return -2;
+		}
+#endif
 		return 0;
 	} catch (...) {
 		assert(false && "Caught exception on incorporate gyros");
@@ -79,8 +195,21 @@ imu_fusion_incorporate_accelerometer(struct imu_fusion *fusion,
 		assert(accel);
 		assert(variance);
 
-		fusion->simple_fusion.handleAccel(
-		    map_vec3(*accel).cast<double>(), dt);
+		if (dt != 0) {
+			flexkalman::predict(fusion->state, fusion->processModel,
+			                    dt);
+		}
+		// the fusion runs on doubles, not floats
+		auto meas = xrt_fusion::WorldDirectionMeasurement<IMUState>(
+		    map_vec3(*accel).cast<double>() * scale,
+		    map_vec3(*reference).cast<double>(),
+		    map_vec3(*variance).cast<double>());
+		if (!flexkalman::correctUnscented(fusion->state, meas)) {
+			fprintf(stderr,
+			        "Non-finite results in correcting fusion using "
+			        "accelerometers - update canceled.\n");
+			return -2;
+		}
 		return 0;
 	} catch (...) {
 		assert(false &&
@@ -99,22 +228,24 @@ imu_fusion_get_prediction(struct imu_fusion const *fusion,
 		assert(fusion);
 		assert(out_quat);
 		assert(out_ang_vel);
-		if (!fusion->simple_fusion.valid()) {
-			return -2;
-		}
 
-		map_vec3(*out_ang_vel) =
-		    fusion->simple_fusion.getAngVel().cast<float>();
 		if (dt == 0) {
 			// No need to predict here.
-			map_quat(*out_quat) =
-			    fusion->simple_fusion.getQuat().cast<float>();
-			return 0;
+			return imu_fusion_output_prediction(
+			    fusion->state, *out_quat, *out_ang_vel);
 		}
-		Eigen::Quaterniond predicted_quat =
-		    fusion->simple_fusion.getPredictedQuat(dt);
-		map_quat(*out_quat) = predicted_quat.cast<float>();
-		return 0;
+
+		// Copy the state so we don't advance the internal clock.
+		IMUState temp{fusion->state};
+		// Copy the process model so we have one to modify.
+		IMUProcessModel process{fusion->processModel};
+
+		// Now, just predict + update the state itself, not the
+		// covariance (save time)
+		flexkalman::predictAndPostCorrectStateOnly(temp, process, dt);
+
+		return imu_fusion_output_prediction(temp, *out_quat,
+		                                    *out_ang_vel);
 
 	} catch (...) {
 		assert(false && "Caught exception on getting prediction");
@@ -131,22 +262,22 @@ imu_fusion_get_prediction_rotation_vec(struct imu_fusion const *fusion,
 		assert(fusion);
 		assert(out_rotation_vec);
 
-		if (!fusion->simple_fusion.valid()) {
-			return -2;
-		}
 		if (dt == 0) {
 			// No need to predict here.
-			map_vec3(*out_rotation_vec) =
-			    fusion->simple_fusion.getRotationVec()
-			        .cast<float>();
-		} else {
-			Eigen::Quaterniond predicted_quat =
-			    fusion->simple_fusion.getPredictedQuat(dt);
-			map_vec3(*out_rotation_vec) =
-			    flexkalman::util::quat_ln(predicted_quat)
-			        .cast<float>();
+			return imu_fusion_output_rot_vec(fusion->state,
+			                                 *out_rotation_vec);
 		}
-		return 0;
+
+		// Copy the state so we don't advance the internal clock.
+		IMUState temp{fusion->state};
+		// Copy the process model so we have one to modify.
+		IMUProcessModel process{fusion->processModel};
+
+		// Now, just predict + update the state itself, not the
+		// covariance (save time)
+		flexkalman::predictAndPostCorrectStateOnly(temp, process, dt);
+
+		return imu_fusion_output_rot_vec(temp, *out_rotation_vec);
 	} catch (...) {
 		assert(false && "Caught exception on getting prediction");
 		return -1;
@@ -171,12 +302,46 @@ imu_fusion_incorporate_gyros_and_accelerometer(
 		assert(accel);
 		assert(accel_reference);
 		assert(accel_variance);
-
-		Eigen::Vector3d accelVec = map_vec3(*accel).cast<double>();
-		Eigen::Vector3d angVelVec = map_vec3(*ang_vel).cast<double>();
-		fusion->simple_fusion.handleAccel(accelVec, dt);
-		fusion->simple_fusion.handleGyro(angVelVec, dt);
-		fusion->simple_fusion.postCorrect();
+		if (dt != 0) {
+			flexkalman::predict(fusion->state, fusion->processModel,
+			                    dt);
+			// Advance the gyro bias state as well: just updates the
+			// variance
+			flexkalman::predict(fusion->gyro_bias.state,
+			                    fusion->gyro_bias.processModel, dt);
+		}
+		{
+			// the fusion runs on doubles, not floats
+			auto meas =
+			    xrt_fusion::WorldDirectionMeasurement<IMUState>(
+			        map_vec3(*accel).cast<double>() * accel_scale,
+			        map_vec3(*accel_reference).cast<double>(),
+			        map_vec3(*accel_variance).cast<double>());
+			if (!flexkalman::correctUnscented(fusion->state,
+			                                  meas)) {
+				fprintf(stderr,
+				        "Non-finite results in correcting "
+				        "fusion using "
+				        "accelerometers - update canceled.\n");
+				return -2;
+			}
+		}
+		{
+			auto meas = xrt_fusion::BiasedGyroMeasurement{
+			    map_vec3(*ang_vel).cast<double>(),
+			    map_vec3(*ang_vel_variance).cast<double>()};
+			auto augmented_state = flexkalman::makeAugmentedState(
+			    fusion->state, fusion->gyro_bias.state);
+			if (!flexkalman::correctUnscented(augmented_state,
+			                                  meas)) {
+				fprintf(stderr,
+				        "Non-finite results in correcting "
+				        "fusion using "
+				        "gyros - "
+				        "update canceled.\n");
+				return -2;
+			}
+		}
 		return 0;
 	} catch (...) {
 		assert(
