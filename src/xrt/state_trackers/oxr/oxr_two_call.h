e1d26cfa4822301dde479c78d26eec2f8f166d89
Christoph Haag <christoph.haag@collabora.com>
2019-12-20 03:00:00 +0100

    st/oxr: Add OXR_TWO_CALL_HELPER_DIRECT macro
    This macro can be used if the requested information is not available
    in a local array and has to be dynamically built.
    In this case it is more convenient to write directly into the output
    array instead of allocating a temporary array.
    However the caller has to handle the capacity correctly.

--- src/xrt/state_trackers/oxr/oxr_two_call.h
+++ src/xrt/state_trackers/oxr/oxr_two_call.h
@@ -37,7 +37,25 @@ extern "C" {
 		return sval;                                                   \
 	} while (false)
 
-
+// The caller writes directly into the output memory and has to handle the
+// cnt_input == 0 and count > cnt_input conditions
+#define OXR_TWO_CALL_HELPER_DIRECT(log, cnt_input, cnt_output, count, sval)    \
+	do {                                                                   \
+		if (cnt_output == NULL) {                                      \
+			return oxr_error(log, XR_ERROR_VALIDATION_FAILURE,     \
+			                 #cnt_output);                         \
+		}                                                              \
+		*cnt_output = count;                                           \
+                                                                               \
+		if (cnt_input == 0) {                                          \
+			return sval;                                           \
+		}                                                              \
+		if (cnt_input < count) {                                       \
+			return oxr_error(log, XR_ERROR_SIZE_INSUFFICIENT,      \
+			                 #cnt_input);                          \
+		}                                                              \
+		return sval;                                                   \
+	} while (false)
 
 #ifdef __cplusplus
 }
