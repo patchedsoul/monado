fab8b851ae889ae0589f8ca5f8a47965f4e22afe
Jakob Bornecrantz <jakob@collabora.com>
2020-02-26 10:02:09 +0000

    WIP: t/calib: Add json configuration parsing and writing

---
+++ src/xrt/auxiliary/tracking/t_calibration_json.c
@@ -0,0 +1,8 @@
+// Copyright 2020, Collabora, Ltd.
+// SPDX-License-Identifier: BSL-1.0
+/*!
+ * @file
+ * @brief  Calibration data to JSON code.
+ * @author Jakob Bornecrantz <jakob@collabora.com>
+ * @ingroup aux_tracking
+ */
